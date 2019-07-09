aa3415a9eb98892033acf138ff7c9d5c1da27ccb
Dragutin Horvat <dhorvat22@gmail.com>
2019-07-09 21:28:18 +0200

    add comment in sql script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO role (id, name) values(1,'ROLE_USER');
 INSERT INTO role (id, name) values(2,'ROLE_ADMIN');
-#pass 123456
+/*pass 123456*/
 INSERT INTO user (id, first_name, last_name, email, password) VALUES (1, 'Dragutin', 'Horvat', 'drago@net.hr', '$2a$10$e2YQ8XsCKkfCDhVYH/VxA.cA36bNVZjftSviUgs6hrl/Gw5jSrswe');
 
 INSERT INTO user_role(user_id, role_id) VALUES (1,2);
