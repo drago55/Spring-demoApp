8d572342b75ee4e697635b185c8d07f79d20eab7
Dragutin Horvat <dhorvat22@gmail.com>
2019-07-10 21:20:22 +0200

    fixed missing "/" in controller

--- src/main/java/com/drago/spring/demo/controllers/AboutController.java
+++ src/main/java/com/drago/spring/demo/controllers/AboutController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AboutController {
 
-	@GetMapping("about")
+	@GetMapping("/about")
 	public String showAbout() {
 		return "about/index";
 	}
