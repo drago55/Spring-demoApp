6a2c6955587047ffb4d9dcaaf00ceb73f5e96b07
Drago <dhorvat22@gmail.com>
2019-08-19 14:55:10 +0200

    Added pageable for users

--- src/main/java/com/drago/spring/demo/controllers/AdministrationController.java
+++ src/main/java/com/drago/spring/demo/controllers/AdministrationController.java
@@ -1,6 +1,8 @@
 package com.drago.spring.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +17,15 @@ public class AdministrationController {
 	private UserService userService;
 
 	@GetMapping(value = "/users")
-	public String showUsers(Model model) {
-		model.addAttribute("users", userService.findAll());
+	public String showUsers(Model model, @PageableDefault(size = 5) Pageable pageable) {
+		model.addAttribute("users", userService.findPaginatedUsers(pageable));
 		return "admin/showUsers";
 	}
 
 	@GetMapping(value = "/user/delete/{id}")
 	public String deleteUser(@PathVariable Long id, Model model) {
-		return "";
+		userService.deleteUser(id);
+		return "redirect:/users";
 	}
 
 	@GetMapping("/edit/{id}")
